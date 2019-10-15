c0870a5ccfbc2cf5700e9301b6cc02e5a355e9f2
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-15 20:54:29 +0200

    Added variables

--- VehicleJdbc/src/test/java/at/htl/vehicle/VehicleTest.java
+++ VehicleJdbc/src/test/java/at/htl/vehicle/VehicleTest.java
@@ -1,4 +1,11 @@
 package at.htl.vehicle;
 
+import java.sql.Connection;
+
 public class VehicleTest {
+    public static final String DRIVER_STRING = "org.apache.derby.jdbc.ClientDriver";
+    static final String CONNECTION_STRING = "jdbc:derby://localhost:1527/db";
+    static final String USER = "app";
+    static final String PASSWORD = "app";
+    private static Connection conn;
 }
