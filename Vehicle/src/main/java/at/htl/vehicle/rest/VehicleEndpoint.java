227e1854965d215a4618cd2880be1f95bf29226f
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-16 09:28:13 +0200

    Added Method find

--- Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
+++ Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
@@ -1,4 +1,17 @@
 package at.htl.vehicle.rest;
 
+import at.htl.vehicle.entity.Vehicle;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("vehicle")
 public class VehicleEndpoint {
+
+    @GET
+    @Path("{id}")
+    public Vehicle find(@PathParam("id") long id){
+        return new Vehicle("Opel " + id, "Commodore");
+    }
 }
