3d0223ff03bb3a08ca659a0f83126f1f9d530c06
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-16 11:46:56 +0200

    Added method fetchVehicle

--- Vehicle-st/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
+++ Vehicle-st/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
@@ -1,10 +1,16 @@
 package at.htl.vehicle.rest;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class VehicleEndpointIT {
     private Client client;
@@ -14,5 +20,13 @@ public class VehicleEndpointIT {
     public void initClient(){
         this.client = ClientBuilder.newClient();
         this.target = client.target("http://localhost:8080/vehicle/rs/vehicle")
+    }
+
+    @Test
+    public void fetchVehicle(){
+        Response response = this.target.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(),is(200));
+        String payload = response.readEntity(String.class);
+        System.out.println("payload = " + payload);
     }
 }
