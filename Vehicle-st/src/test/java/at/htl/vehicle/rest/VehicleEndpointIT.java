8ada125a1d17d912d773e776911093c90c8158a6
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-16 10:59:00 +0200

    Added method initClient

--- Vehicle-st/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
+++ Vehicle-st/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
@@ -1,4 +1,18 @@
 package at.htl.vehicle.rest;
 
+import org.junit.Before;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 public class VehicleEndpointIT {
+    private Client client;
+    private WebTarget target;
+
+    @Before
+    public void initClient(){
+        this.client = ClientBuilder.newClient();
+        this.target = client.target("http://localhost:8080/vehicle/rs/vehicle")
+    }
 }
