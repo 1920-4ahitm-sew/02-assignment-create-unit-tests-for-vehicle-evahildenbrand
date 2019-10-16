cfa0b6a5fdcd8fcd5c2c059d8f03aac3a2d246e3
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-16 17:57:02 +0200

    Added method for deleting an Vehicle

--- Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
+++ Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
@@ -2,6 +2,7 @@ package at.htl.vehicle.rest;
 
 import at.htl.vehicle.entity.Vehicle;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -22,5 +23,11 @@ public class VehicleEndpoint {
         List<Vehicle> all = new ArrayList<>();
         all.add(find(42));
         return all;
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") long id){
+        System.out.println("deleted = " + id);
     }
 }
