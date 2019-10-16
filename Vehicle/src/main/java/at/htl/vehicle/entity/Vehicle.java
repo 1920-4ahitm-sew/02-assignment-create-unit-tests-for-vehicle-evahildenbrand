44b7892299e6145621383e93a45cacb1a48cb954
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-16 09:24:03 +0200

    Added Getter and Setter

--- Vehicle/src/main/java/at/htl/vehicle/entity/Vehicle.java
+++ Vehicle/src/main/java/at/htl/vehicle/entity/Vehicle.java
@@ -1,4 +1,24 @@
 package at.htl.vehicle.entity;
 
 public class Vehicle {
+    String brand;
+    String type;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
+
+
