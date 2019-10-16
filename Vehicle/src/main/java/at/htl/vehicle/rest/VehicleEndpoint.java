4a799b5fee18e4d537066419aa4fe5d2f45ba8df
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-16 09:30:24 +0200

    Added Methodd findAll

--- Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
+++ Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
@@ -5,6 +5,8 @@ import at.htl.vehicle.entity.Vehicle;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("vehicle")
 public class VehicleEndpoint {
@@ -13,5 +15,12 @@ public class VehicleEndpoint {
     @Path("{id}")
     public Vehicle find(@PathParam("id") long id){
         return new Vehicle("Opel " + id, "Commodore");
+    }
+
+    @GET
+    public List<Vehicle> findAll(){
+        List<Vehicle> all = new ArrayList<>();
+        all.add(find(42));
+        return all;
     }
 }
